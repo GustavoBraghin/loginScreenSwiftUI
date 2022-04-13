a43209b599514f40090fd329b685448454b99228
GustavoBraghin <gugabraghin@gmail.com>
2022-04-13 15:23:53 -0300

    Initial Commit

---
+++ loginScreenSwiftUI/loginScreenSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginScreenSwiftUIApp.swift
+//  loginScreenSwiftUI
+//
+//  Created by Gustavo da Silva Braghin on 13/04/22.
+//
+
+import SwiftUI
+
+@main
+struct loginScreenSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
