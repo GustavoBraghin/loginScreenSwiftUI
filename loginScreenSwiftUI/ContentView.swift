8943b8c2307c161e777831def02359128c3cb971
GustavoBraghin <gugabraghin@gmail.com>
2022-04-13 15:49:50 -0300

    added basic components to view

--- loginScreenSwiftUI/ContentView.swift
+++ loginScreenSwiftUI/ContentView.swift
@@ -8,9 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var username: String = ""
+    @State var password: String = ""
+    
+    let bound = UIScreen.main.bounds
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack{
+            
+            Text("First Login Screen")
+                .font(.title)
+                .foregroundColor(.white)
+                
+            
+            TextField("Username", text: $username)
+                .textFieldStyle(.roundedBorder)
+                .padding(.horizontal)
+            
+            TextField("Password", text: $password)
+                .textFieldStyle(.roundedBorder)
+                .padding(.horizontal)
+            
+            Button("Login", action: {
+                print("login")
+            })
+            .padding(5)
+            .background(.blue)
+            .foregroundColor(.white)
+            
+            Text("or connect with")
+                .font(.footnote)
+                .foregroundColor(.white)
+            
+            HStack{
+                Button("FB", action: {
+                    print("FB")
+                })
+                .padding(5)
+                .foregroundColor(.white)
+                .background(.blue)
+                
+                Button("TT", action: {
+                    print("TT")
+                })
+                .padding(5)
+                .foregroundColor(.white)
+                .background(.cyan)
+                
+            }.padding(5)
+        }
+        .frame(width: bound.width, height: bound.height, alignment: .center)
+        .background(.purple)
     }
 }
 
